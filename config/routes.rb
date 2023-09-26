7afe9e1ea54787c908383529b87aebc9c34bb923
Estifanos <estifanosbeyene21@gmail.com>
2023-09-26 10:21:49 -0400

    feat: clean up
    use link to instead of anchor tag
    use resources instead of individual routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
   # Defines the root path route ("/")
   # root "articles#index"
 end
